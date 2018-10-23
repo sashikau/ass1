8f158d83f0db7eb611f7f3e06d5690c4695e94da
sashikaudana <sashikaudana@ymail.com>
2018-10-23 05:52:36 +0530

    initial commmit

---
+++ src/main/java/com/cmb/app/App.java
@@ -0,0 +1,13 @@
+package com.cmb.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
